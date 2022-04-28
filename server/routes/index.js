32d1034e3cd4c05573bdc0f622ec671f0f4bdaae
Sabina <Sabinagli45@gmail.com>
2022-04-28 17:07:45 +0300

    init

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const Router = require("express");
+const router = new Router();
+
+const brandRouter = require("./brandRouter");
+const itemRouter = require("./itemRouter");
+const typeRouter = require("./typeRourer");
+const userRouter = require("./userRouter");
+
+router.use("/user", userRouter);
+router.use("/type", typeRouter);
+router.use("/brand", brandRouter);
+router.use("/item", itemRouter);
+
+module.exports = router;
