b98412d2a667e7e0b67ecaa4d460e86cfd2e2e41
Sabina <Sabinagli45@gmail.com>
2022-04-28 20:15:26 +0300

    crud brand type

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,5 @@
 const Router = require("express");
 const router = new Router();
-
 const brandRouter = require("./brandRouter");
 const itemRouter = require("./itemRouter");
 const typeRouter = require("./typeRourer");
