431dd6cc6a4318c41bd17c005bc121fae11088cb
Sabina <Sabinagli45@gmail.com>
2022-04-28 18:30:21 +0300

    generic error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,16 @@
+const ApiError = require("../error/apiError");
+
 class UserController {
   async registration(req, res) {}
 
   async login(req, res) {}
 
-  async check(req, res) {
-    const query = res.query;
-    res.json(query);
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest("not set id"));
+    }
+    res.json(id);
   }
 }
 
